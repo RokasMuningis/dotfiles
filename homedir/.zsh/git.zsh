6f424b800cbf6f554e33d4180e8a39ffb48e8f42
Rokas Muningis <28229273+RokasMuningis@users.noreply.github.com>
2019-08-04 23:56:46 +0300

    Add git aliases

---
+++ homedir/.zsh/git.zsh
@@ -0,0 +1,19 @@
+alias ga="git add"
+alias gaa="git add ."
+
+alias gp="git push"
+alias gpo="git push -u origin"
+alias gpd="git push -d origin"
+
+alias gck="git checkout"
+alias gckb="git checkout -b"
+
+alias gb="git branch"
+alias gbd="git branch -D"
+
+alias gc="git commit"
+alias gcm="git commit -m"
+
+alias gss="git status -s"
+alias gl="git log"
+alias gll="git log --graph --oneline"
